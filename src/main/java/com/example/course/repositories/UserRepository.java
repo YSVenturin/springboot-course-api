38c59d78906b83963eb5be19a3f0ebe5a97f55a3
Yuri Sabedot Venturin <ysventurin@ucs.br>
2026-07-06 14:55:50 -0300

    feat: add service layer, component registration

--- src/main/java/com/example/course/repositories/UserRepository.java
+++ src/main/java/com/example/course/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package com.example.course.repositories;
 
 import com.example.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
--- src/main/java/com/example/course/resources/UserResource.java
+++ src/main/java/com/example/course/resources/UserResource.java
@@ -1,18 +1,34 @@
 package com.example.course.resources;
 
 import com.example.course.entities.User;
+import com.example.course.services.UserService;
+import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.cdi.Eager;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "Maria", "maria@mail.com", "5499999999", "99999");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> users = userService.findAll();
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(user);
     }
 }
